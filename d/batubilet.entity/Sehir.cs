01cf15e276e7e7212576eec896d0e701e9441896
aliyilmazlar <122096066+aliyilmazlar@users.noreply.github.com>
2023-01-17 23:16:27 +0300

    butun dosyalar

---
+++ d/batubilet.entity/Sehir.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace batubilet.entity
+{
+    public class Sehir
+    {
+        public int SehirId { get; set; }
+        public string SehirAd { get; set; }
+        
+    }
+}
+
